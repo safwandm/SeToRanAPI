5f6bfcbcf9827c6d6e14d9492655b57df550dd13
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2024-12-25 16:27:24 +0700

    Add Mitra and Motor seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PenggunaSeeder::class,
             PelangganSeeder::class,
+            MitraSeeder::class,
+            MotorSeeder::class,
         ]);
     }
 }

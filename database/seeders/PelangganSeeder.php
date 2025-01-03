51c22ce2f6ebe3c3efd032017d0ba095e1f4aabe
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2025-01-04 00:13:36 +0700

    Refactor PelangganSeeder and PenggunaSeeder to use manual data instead of faker

--- database/seeders/PelangganSeeder.php
+++ database/seeders/PelangganSeeder.php
@@ -20,17 +20,13 @@ class PelangganSeeder extends Seeder
         // Get all pengguna IDs
         $penggunaIds = DB::table('penggunas')->pluck('id_pengguna');
         
-        // Create pelanggan records for some of the penggunas
         foreach ($penggunaIds as $penggunaId) {
-            // Randomly skip some penggunas (70% chance to create a pelanggan)
-            if ($faker->boolean(70)) {
-                DB::table('pelanggans')->insert([
-                    'id_pengguna' => $penggunaId,
-                    'nomor_SIM' => $faker->numerify('##########'), // 10 digit SIM
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
-            }
+            DB::table('pelanggans')->insert([
+                'id_pengguna' => $penggunaId,
+                'nomor_SIM' => $faker->numerify('##########'), // 10 digit SIM
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
         }
     }
 }
